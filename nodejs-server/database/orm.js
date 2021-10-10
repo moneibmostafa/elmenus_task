bcbb71b209680dec3a11717a384193d04f47b9c9
mostafamoneib <mostafa.moneib@ibm.com>
2021-10-10 23:07:54 +0200

    add database constraint

--- nodejs-server/database/orm.js
+++ nodejs-server/database/orm.js
@@ -2,33 +2,36 @@ const Sequelize = require('sequelize');
 const config = require('../config');
 const { logger } = require('../logger');
 
-if (process.env.NODE_ENV === 'development') {
-  setTimeout(() => {
-    const sequelize = new Sequelize(
-      `${config.db.dialect}://${config.db.username}:${config.db.password}@${config.db.host}:${config.db.port}/${config.db.database}`,
-      {
-        logging: function (str) {
-          logger.log('info', `Sequelize: ${str}`);
-        },
-      }
+let sequelize;
+
+const databaseInit = () => {
+  sequelize = new Sequelize(
+    `${config.db.dialect}://${config.db.username}:${config.db.password}@${config.db.host}:${config.db.port}/${config.db.database}`,
+    {
+      logging: function (str) {
+        logger.log('info', `Sequelize: ${str}`);
+      },
+    }
+  );
+
+  sequelize
+    .authenticate()
+    .then(() => {
+      logger.log('info', 'Database connection has been established successfully.');
+    }
+    )
+    .catch((error) => {
+      logger.log('error', 'Unable to connect to the database:', error);
+      setTimeout(() => databaseInit(), 5000);
+    }
     );
-    sequelize
-      .authenticate()
-      .then(() => {
-        logger.log('info', 'Database connection has been established successfully.');
-        connected = true;
-      }
-      )
-      .catch((error) => {
-        logger.log('error', 'Unable to connect to the database:', error);
-      }
-      );
-    
-    sequelize
-      .sync({ alter: true })
-      .then(() => logger.log('info', 'sync successful.'))
-      .catch((error) => logger.log('error', 'sync failure', error));
-  }, 10000);
+  
+  sequelize
+    .sync({ alter: true })
+    .then(() => logger.log('info', 'sync successful.'))
+    .catch((error) => logger.log('error', 'sync failure', error));
 }
+
+databaseInit();
 
 module.exports = sequelize;
